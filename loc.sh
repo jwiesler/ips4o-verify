a9c4d0381d34d1c189a26bfcb510295f182c756a
Mattias Ulbrich <ulbrich@kit.edu>
2023-10-14 01:18:47 +0200

    cleaning the repo

--- loc.sh
+++
@@ -1,6 +0,0 @@
-for f in src/de/wiesler/*.java; do
-    spec=$(cat "$f" | egrep "@" | egrep -v "@\s*$" | egrep -v "@ //" | wc -l)
-	code=$(cat "$f" | egrep -v "(@|^\s+$)" | wc -l)
-	base=$(basename $f)
-	echo "$base,$code,$spec"
-done
